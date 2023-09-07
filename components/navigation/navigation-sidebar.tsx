9706b474398a129c5c9386f1c823e7bd72f53d38
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-09-07 09:21:07 +0430

    adding tooltip

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,12 +1,25 @@
 import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/navigation";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
   if (!profile) {
-    return null;
+    redirect("/");
   }
-  console.log(profile);
-  return <div>NavigationSidebar</div>;
+  const servers = await prisma?.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-primary dark:bg-[#1E1F22]">
+      NavigationSidebar
+    </div>
+  );
 };
 
 export default NavigationSidebar;
