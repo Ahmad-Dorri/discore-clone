e97a190c6a749659e25db8bbf8e61680413b0c76
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-09-09 11:45:09 +0430

    adding the route logic for updating the profile

--- app/api/profile/[id]/route.ts
+++ app/api/profile/[id]/route.ts
@@ -1,6 +1,12 @@
 import { verifyJwt } from "@/lib/jwt";
 import { prisma } from "@/lib/prisma";
 
+interface RequestBody {
+  name: string;
+  email: string;
+  imageUrl: string;
+}
+
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
@@ -20,6 +26,37 @@ export async function GET(
   const profile = await prisma.profile.findFirst({
     where: {
       id: params.id,
+    },
+  });
+
+  return new Response(JSON.stringify(profile));
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const accessToken = request.headers.get("authorization");
+  const body: RequestBody = await request.json();
+  console.log(body);
+  if (!accessToken || !verifyJwt(accessToken)) {
+    return new Response(
+      JSON.stringify({
+        error: "unauthorized",
+      }),
+      {
+        status: 401,
+      },
+    );
+  }
+  const profile = await prisma.profile.updateMany({
+    where: {
+      id: params.id,
+    },
+    data: {
+      name: body.name,
+      email: body.email,
+      imageUrl: body.imageUrl,
     },
   });
 
