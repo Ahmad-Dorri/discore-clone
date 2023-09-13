e9c00ad2e11dd718bc1931dce84214bca710854d
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-09-13 10:57:48 +0430

    adding ui for changing the user role and kick users to manage-members-modal

--- components/modals/manage-members-modal.tsx
+++ components/modals/manage-members-modal.tsx
@@ -1,11 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import {
+  Check,
+  Gavel,
+  Loader,
+  Loader2,
+  MoreVertical,
+  Shield,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion,
+} from "lucide-react";
 
 import Modal from "@/components/ui/modal";
 import { onClose } from "@/store/slices/modal-slice";
 import { RootState } from "@/store/store";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import UserAvatar from "@/components/user-avatar";
-import { ShieldAlert, ShieldCheck } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const roleIconMap = {
   ADMIN: <ShieldAlert className=" h-4 w-4 text-rose-500" />,
@@ -16,6 +38,7 @@ const roleIconMap = {
 const ManageMembersModal = () => {
   const modal = useSelector((state: RootState) => state.modal);
   const dispatch = useDispatch();
+  const [loadingId, setLoadingId] = useState("");
   const isModalOpen = modal.isOpen && modal.type === "ManageMembers";
 
   const { server } = modal.data;
@@ -41,6 +64,50 @@ const ManageMembersModal = () => {
               </div>
               <p className="text-xs text-zinc-500">{member.profile.email}</p>
             </div>
+            {server.profileId !== member.profileId &&
+              loadingId !== member.id && (
+                <div className="ml-auto">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVertical className="h-4 w-4 text-zinc-500 " />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side="left">
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger className="flex items-center">
+                          <ShieldQuestion className="mr-2 h-4 w-4" />
+                          <span>Role</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem>
+                              <Shield className="mr-2 h-4 w-4" />
+                              Guest
+                              {member.role === "GUEST" && (
+                                <Check className="ml-auto h-4 w-4" />
+                              )}
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                              <ShieldCheck className="mr-2 h-4 w-4" />
+                              Moderator
+                              {member.role === "MODERATOR" && (
+                                <Check className="ml-auto h-4 w-4" />
+                              )}
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem className="text-rose-500">
+                        <Gavel className="mr-2 h-4 w-4 text-rose-500" />
+                        Kick
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              )}
+            {loadingId === member.id && (
+              <Loader2 className="ml-auto h-4 w-4 animate-spin text-zinc-500" />
+            )}
           </div>
         ))}
       </ScrollArea>
